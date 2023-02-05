70493a58ada1a91bcb1a08b8f0739b321c3d7742
mihai-radulescu <62609189+mihai-radulescu@users.noreply.github.com>
2023-02-05 17:46:43 +0200

    Added a search bar

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -1,8 +1,10 @@
 import NotesList from "./components/NotesList";
+import Search from "./components/Search";
 
 const App = () =>{
   return (
     <div class='container'>
+      <Search />
       <NotesList />
     </div>
   );
